09446675d028af74cefe4baaf8efac121af0d964
Dmytro Grunytsky <dima.grunitsky@gmail.com>
2020-10-20 16:06:36 +0300

    Сума від'ємних елементів масиву

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    { 
+           
+            
+        static void Main(string[] args)
+        {
+            //сума від'ємних елементів масиву 
+            int A[50];
+            int suma;// змінна цієї суми
+            int i;//додаткова змінна
+                  // вивід масиву
+                  //...
+                  //обчисленння суми 
+            suma = 0;//обнулити суму
+            for (i = 0; i < 50; i++)
+                if (A[i] > 0)
+                    suma = suma + A[i];
+
+        }
+    }
+}
